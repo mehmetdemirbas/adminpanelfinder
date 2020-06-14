c0bbfbbaf9e95de05a02d0ff6d7ab4da230f3c73
mehmetdemirbas <66621446+mehmetdemirbas@users.noreply.github.com>
2020-06-14 20:39:32 +0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,9 @@
+::: Admin Panel Scan :::
+
+python adminfinder.py
+
+www.example.com
+
+contact : instagram.com/slayeri4.php
+          mail : slayeri4.py@gmail.com
+          twitter.com/Hackerxp21
---
+++ adminfinder.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+from urllib2 import Request, urlopen, URLError, HTTPError
+
+def Space(j):
+	i = 0
+	while i<=j:
+		print " ",
+		i+=1
+
+
+def findAdmin():
+	f = open("link.txt","r");
+	link = raw_input("Site Adresi \n(örnek : hedefsite.com or www.hedefsite.com ): ")
+	print "\n\nAvilable links : \n"
+	while True:
+		sub_link = f.readline()
+		if not sub_link:
+			break
+		req_link = "http://"+link+"/"+sub_link
+		req = Request(req_link)
+		try:
+			response = urlopen(req)
+		except HTTPError as e:
+			continue
+		except URLError as e:
+			continue
+		else:
+			print "OK => ",req_link
+
+def Credit():
+	Space(9); print "#####################################"
+	Space(9); print "#   +++   Admin Panel Finder   +++   #"
+	Space(9); print "#     Hazirlayan : Mehmet Demirbaş   #"
+	Space(9); print "#   intagram : @root.mehmet    #"
+	Space(9); print "#####################################"
+
+Credit()
+findAdmin()
